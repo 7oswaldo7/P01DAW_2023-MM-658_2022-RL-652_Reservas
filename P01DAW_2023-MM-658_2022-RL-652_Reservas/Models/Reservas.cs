ce3896c5e80597e811104330fe30ed50795a266d
Juan José <juanjosemagaa@hotmail.com>
2025-02-27 18:37:22 -0600

    Modelos hechos

--- P01DAW_2023-MM-658_2022-RL-652_Reservas/Models/Reservas.cs
+++ P01DAW_2023-MM-658_2022-RL-652_Reservas/Models/Reservas.cs
@@ -1,6 +1,49 @@
-﻿namespace P01DAW_2023_MM_658_2022_RL_652_Reservas.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace P01DAW_2023_MM_658_2022_RL_652_Reservas.Models
 {
-    public class Reservas
+    public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
     }
+
+    public class Sucursal
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int AdministradorId { get; set; }
+        public int EspaciosTotales { get; set; }
+
+    }
+
+    public class EspacioParqueo
+    {
+        [Key]
+        public int Id { get; set; }
+        public int SucursalId { get; set; }
+        public int Numero { get; set; }
+        public string Ubicacion { get; set; }
+        public decimal CostoPorHora { get; set; }
+        public string Estado { get; set; }
+    }
+
+    public class Reserva
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int EspacioParqueoId { get; set; }
+        public DateTime FechaHoraInicio { get; set; }
+        public int HorasReservadas { get; set; }
+        public string Estado { get; set; }
+    }
+
 }
