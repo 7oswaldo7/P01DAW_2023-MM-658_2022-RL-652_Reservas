4829b5aad33c8a1d74f7ceddaae608836dcb9c3e
Juan José <juanjosemagaa@hotmail.com>
2025-02-27 20:12:38 -0600

    Gestor de Usuarios Completado

--- P01DAW_2023-MM-658_2022-RL-652_Reservas/Controllers/GestorUsuariosController.cs
+++ P01DAW_2023-MM-658_2022-RL-652_Reservas/Controllers/GestorUsuariosController.cs
@@ -46,5 +46,61 @@ namespace P01DAW_2023_MM_658_2022_RL_652_Reservas.Controllers
                 return Ok($"Bienvenido/a {validacionUsuario.Nombre}");
 
         }
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarEquipo([FromBody] Usuarios usuario)
+        {
+            try
+            {
+                _reservasContexto.usuarios.Add(usuario);
+                _reservasContexto.SaveChanges();
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Actualizar/{id}")]
+        public IActionResult ActualizarUsuario(int id, [FromBody] Usuarios UsuarioModificar)
+        {
+            Usuarios? usuarioActual = (from e in _reservasContexto.usuarios where e.Id == id select e).FirstOrDefault();
+
+            if (usuarioActual == null)
+            {
+                return NotFound();
+            }
+
+            usuarioActual.Nombre = UsuarioModificar.Nombre;
+            usuarioActual.Rol = UsuarioModificar.Rol;
+            usuarioActual.Telefono = UsuarioModificar.Telefono;
+            usuarioActual.Correo = UsuarioModificar.Correo;
+            usuarioActual.Contrasena = UsuarioModificar.Contrasena;
+
+            _reservasContexto.Entry(usuarioActual).State = EntityState.Modified;
+            _reservasContexto.SaveChanges();
+            return Ok(UsuarioModificar);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult EliminarUsuario(int id)
+        {
+            Usuarios? usuario = (from e in _reservasContexto.usuarios where e.Id == id select e).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _reservasContexto.usuarios.Attach(usuario);
+            _reservasContexto.usuarios.Remove(usuario);
+            _reservasContexto.SaveChanges();
+
+            return Ok(usuario);
+        }
     }
 }
