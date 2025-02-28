78fe92c82965816e275617bb568a3840a03f6287
Juan José <juanjosemagaa@hotmail.com>
2025-02-27 20:00:53 -0600

    Varios Cambios Realizados

--- BASE DE DATOS.sql
+++ BASE DE DATOS.sql
@@ -1,45 +1,52 @@
-create database Reservas
+create database PARQUEOSDB 
 go
-use Reservas
-CREATE TABLE Usuario (
+use PARQUEOSDB 
+CREATE TABLE Usuarios (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre NVARCHAR(100) NOT NULL,
-    Correo NVARCHAR(255) UNIQUE NOT NULL,
-    Telefono NVARCHAR(15) NOT NULL,
-    Contrasena NVARCHAR(255) NOT NULL,
-    Rol NVARCHAR(15) CHECK (Rol IN ('Cliente', 'Empleado', 'Administrador')) NOT NULL
+    Nombre varchar(100) NOT NULL,
+    Correo VARCHAR(255) UNIQUE NOT NULL,
+    Telefono VARCHAR(15) NOT NULL,
+    Contrasena VARCHAR(255) NOT NULL,
+    Rol VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE Sucursal (
+CREATE TABLE Sucursales (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre NVARCHAR(100) NOT NULL,
-    Direccion NVARCHAR(255) NOT NULL,
-    Telefono NVARCHAR(15) NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Direccion VARCHAR(255) NOT NULL,
+    Telefono VARCHAR(15) NOT NULL,
     AdministradorId INT NOT NULL,
-    EspaciosTotales INT NOT NULL CHECK (EspaciosTotales >= 0),
-    CONSTRAINT FK_Sucursal_Administrador FOREIGN KEY (AdministradorId) REFERENCES Usuario(Id) 
+    EspaciosTotales INT NOT NULL,
+    CONSTRAINT FK_Sucursal_Administrador FOREIGN KEY (AdministradorId) REFERENCES Usuarios(Id) 
         ON DELETE NO ACTION ON UPDATE CASCADE
 );
-CREATE TABLE EspacioParqueo (
+CREATE TABLE EspacioParqueos (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     SucursalId INT NOT NULL,
     Numero INT NOT NULL,
-    Ubicacion NVARCHAR(100) NOT NULL,
-    CostoPorHora DECIMAL(10,2) NOT NULL CHECK (CostoPorHora >= 0),
-    Estado NVARCHAR(10) CHECK (Estado IN ('Disponible', 'Ocupado', 'Reservado')) NOT NULL,
-    CONSTRAINT FK_EspacioParqueo_Sucursal FOREIGN KEY (SucursalId) REFERENCES Sucursal(Id) 
+    Ubicacion VARCHAR(100) NOT NULL,
+    CostoPorHora DECIMAL(10,2) NOT NULL,
+    Estado VARCHAR(10) NOT NULL,
+    CONSTRAINT FK_EspacioParqueo_Sucursal FOREIGN KEY (SucursalId) REFERENCES Sucursales(Id) 
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-CREATE TABLE Reserva (
+CREATE TABLE Reservas (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     UsuarioId INT NOT NULL,
     EspacioParqueoId INT NOT NULL,
     FechaHoraInicio DATETIME NOT NULL,
-    HorasReservadas INT NOT NULL CHECK (HorasReservadas > 0),
-    Estado NVARCHAR(10) CHECK (Estado IN ('Activa', 'Cancelada', 'Finalizada')) NOT NULL,
-    CONSTRAINT FK_Reserva_Usuario FOREIGN KEY (UsuarioId) REFERENCES Usuario(Id) 
+    HorasReservadas INT NOT NULL ,
+    Estado VARCHAR(10) NOT NULL,
+    CONSTRAINT FK_Reserva_Usuario FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id) 
         ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT FK_Reserva_EspacioParqueo FOREIGN KEY (EspacioParqueoId) REFERENCES EspacioParqueo(Id) 
+    CONSTRAINT FK_Reserva_EspacioParqueo FOREIGN KEY (EspacioParqueoId) REFERENCES EspacioParqueos(Id) 
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
+
+
+INSERT INTO [dbo].[Usuarios] ([Nombre], [Correo], [Telefono], [Contrasena], [Rol])
+VALUES 
+('Juan Perez', 'juan.perez@example.com', '1234567890', 'contrasenaSegura123', 'Cliente'),
+('Maria Gomez', 'maria.gomez@example.com', '0987654321', 'contrasenaSegura456', 'Empleado')
+GO
---
+++ P01DAW_2023-MM-658_2022-RL-652_Reservas/Controllers/GestorUsuariosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01DAW_2023_MM_658_2022_RL_652_Reservas.Models;
+
+namespace P01DAW_2023_MM_658_2022_RL_652_Reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestorUsuariosController : ControllerBase
+    {
+        private readonly ReservasContext _reservasContexto;
+        public GestorUsuariosController(ReservasContext reservasContexto)
+        {
+            _reservasContexto = reservasContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            var listadoUsuario = (from e in _reservasContexto.usuarios select e);
+
+
+            if (listadoUsuario.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoUsuario);
+        }
+
+        [HttpGet]
+        [Route("LogIn")]
+        public IActionResult LogIn(string email, string pass)
+        {
+            var validacionUsuario = (from e in _reservasContexto.usuarios
+                                  where e.Contrasena == pass && e.Correo == email
+                                  select e).FirstOrDefault();
+
+
+            if (validacionUsuario == null)
+            {
+                return Unauthorized("Correo o contraseña invalidos");
+            }
+            else
+                return Ok($"Bienvenido/a {validacionUsuario.Nombre}");
+
+        }
+    }
+}
